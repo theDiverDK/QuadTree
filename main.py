1baac255c9781c049bf91d003867e43ef7dae4e6
Søren Reinke <soren@reinke.dk>
2018-04-15 20:48:44 +0200

    update

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-from graphics import GraphWin
-
 import random
-
 from quadtree import QuadTree
 from point import Point
 from rectangle import Rectangle
-from graphics import Rectangle as gRectangle, Point as gPoint
+from graphics import Rectangle as gRectangle, Point as gPoint, GraphWin
 
 
 def main():
@@ -18,7 +15,7 @@ def main():
     rect = Rectangle(x1, y1, x2, y2)
     qt = QuadTree(rect, 4)
 
-    for _ in range(100):
+    for _ in range(1000):
         qt.insert(Point(random.randrange(x1, x2),
                         random.randrange(y1, y2)))
 
@@ -28,12 +25,12 @@ def main():
     mrect = None
     while True:
         mouse = win.checkMouse()
-        print(mouse)
+        
         if not mouse is None:
 
-            if not mrect is None:
-                mrect.undraw(win)
-                print("undraw")
+            # if not mrect is None:
+            #     mrect.undraw(win)
+            #     print("undraw")
             if mouse.x >= border and mouse.x <= width-border and mouse.y >= border and mouse.y <= width-border:
                 mrect = gRectangle(gPoint(mouse.x-border, mouse.y-border), gPoint(mouse.x+border, mouse.y+border))
                 mrect.draw(win)
